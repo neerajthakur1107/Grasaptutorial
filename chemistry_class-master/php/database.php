feadd816385ad944762301d5b9c3211393b7944d
shreya <shreyadabbawar@gmail.com>
2021-12-09 14:44:08 +0530

    finished1345

--- chemistry_class-master/php/database.php
+++ chemistry_class-master/php/database.php
@@ -1,2 +1,2 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'newchatapp') or die("Database Not Connected !");
+$db = mysqli_connect('localhost', 'u560383184_graspTutorials', 'Grasp@2021', 'u560383184_Chemistry') or die("Database Not Connected !");
